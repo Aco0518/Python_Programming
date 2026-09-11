4f921550fce0622701bdc137a47417c9191d9e45
Aco0518 <acooo0518@gmail.com>
2026-09-11 14:17:32 +0900

    마지막 코드 추가해서 다시 커밋..

--- 04_collection/ex4.py
+++ 04_collection/ex4.py
@@ -140,3 +140,11 @@ tot = sum([price * qty for _, price, qty in stocks])
 
 print(f"총액: {tot:,}원")
 # ✅ 총액: 21,000원
+
+stocks = (
+    ("사과", "바나나", "체리"),
+    (1000, 2000, 5000),
+    (5, 3, 2),
+)
+tot = sum(price * qty for _, price, qty in zip(*stocks))
+print(tot)
